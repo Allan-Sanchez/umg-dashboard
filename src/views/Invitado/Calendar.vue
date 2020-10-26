b1f4a50c66d358de5cd72210e024c015b5cee4ca
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-10-26 13:16:21 -0600

    change and page guest calendar and events

--- src/views/Invitado/Calendar.vue
+++ src/views/Invitado/Calendar.vue
@@ -1,10 +1,63 @@
 <template>
+<v-row class="justify-center">
+  <v-col md="10">
+    <v-form>
+      <v-row>
+        <v-col>
+       <v-autocomplete
+        :items="states"
+        color="white"
+        v-model="facultad"
+        item-text="name"
+        label="Selecciona tu facultad Facultad"
+      ></v-autocomplete>
+        </v-col>
+        <v-col>
+          <v-autocomplete
+        :items="states"
+        color="white"
+        item-text="name"
+        label="Selecciona el nombre de tu profesor"
+      ></v-autocomplete>
+        </v-col>
+        <v-col>
+          <v-autocomplete
+        :items="states"
+        color="white"
+        item-text="name"
+        label="Selecciona el nombre del curso"
+      ></v-autocomplete>
+
+        </v-col>
+      </v-row>
+    </v-form>
   <h2>Calendar</h2>
+  <Calendar></Calendar>
+
+  </v-col>
+</v-row>
 </template>
 
 <script>
+import Calendar from "@/components/UI/Calendar.vue"
 export default {
-name:"guess-calendar"
+
+name:"guess-calendar",
+components:{
+  Calendar
+},
+data() {
+  return {
+    facultad:'',
+    states: [
+          { name: 'Florida', abbr: 'FL', id: 1 },
+          { name: 'Georgia', abbr: 'GA', id: 2 },
+          { name: 'Nebraska', abbr: 'NE', id: 3 },
+          { name: 'California', abbr: 'CA', id: 4 },
+          { name: 'New York', abbr: 'NY', id: 5 },
+        ],
+  }
+},
 }
 </script>
 
--- src/views/Invitado/Events.vue
+++ src/views/Invitado/Events.vue
@@ -1,13 +1,130 @@
 <template>
-  <h2>Events</h2>
+  <v-row class="justify-center">
+    <v-col md="8">
+      <v-card>
+        <v-card-text>
+          <div class="image-post" style="background-image: url('https://images.unsplash.com/photo-1602526215099-19d4d14797d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')">
+            <!-- <img src="../../assets/UMG.png" alt=""> -->
+          </div>
+          <div class="mt-5  d-md-flex justify-md-space-between title-blog">
+            <div>
+              <h2 class="blue--text font-weight-bold text-h4">Lorem, ipsum dolor.</h2>
+              <span class="text-subtitle-2"><b class="blue--text">creado por:</b> Lorem, ipsum dolor.</span>
+            </div>
+            <div>
+              <v-chip class="ma-2" color="red" text-color="white">
+                <span>20/10/2020</span>
+              </v-chip>
+            </div>
+          </div>
+          <div class="paragraf-blog">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+              pariatur nemo quaerat laborum harum, consectetur ullam. Sint nam
+              commodi ullam repellat obcaecati voluptate ab delectus quisquam
+              nisi, porro dolorum eos?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+              pariatur nemo quaerat laborum harum, consectetur ullam. Sint nam
+              commodi ullam repellat obcaecati voluptate ab delectus quisquam
+              nisi, porro dolorum eos?
+            </p>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-bookmark</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    
+      <v-col md="8">
+      <v-card>
+        <v-card-text>
+          <div class="image-post" style="background-image: url('https://images.unsplash.com/photo-1603655889313-bdf00862dd1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')">
+            <!-- <img src="../../assets/UMG.png" alt=""> -->
+          </div>
+          <div class="mt-5 d-md-flex justify-md-space-between title-blog">
+            <div>
+              <h2 class="blue--text font-weight-bold text-h4">Lorem, ipsum dolor.</h2>
+              <span class="text-subtitle-2"><b class="blue--text">creado por:</b> Lorem, ipsum dolor.</span>
+            </div>
+            <div>
+              <v-chip class="ma-2" color="red" text-color="white">
+                <span>20/10/2020</span>
+              </v-chip>
+            </div>
+          </div>
+          <div class="paragraf-blog">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+              pariatur nemo quaerat laborum harum, consectetur ullam. Sint nam
+              commodi ullam repellat obcaecati voluptate ab delectus quisquam
+              nisi, porro dolorum eos?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+              pariatur nemo quaerat laborum harum, consectetur ullam. Sint nam
+              commodi ullam repellat obcaecati voluptate ab delectus quisquam
+              nisi, porro dolorum eos?
+            </p>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-bookmark</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    
+  </v-row>
 </template>
 
 <script>
 export default {
-    name:"guest-event"
-}
+  name: "guest-event",
+};
 </script>
 
 <style>
-
+.image-post {
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  /* background-image: url("../../assets/UMG.png"); */
+  /* background-image: url("https://images.unsplash.com/photo-1603655889313-bdf00862dd1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"); */
+  background-position: center;
+  background-size: contain;
+  max-height: 400px;
+}
+.title-blog{
+  padding: 1rem 2rem;
+}
+.paragraf-blog{
+  padding: 1rem 2rem;
+  text-align: justify;
+}
 </style>
