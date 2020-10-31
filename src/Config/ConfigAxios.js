b4dd6acf8b5982225395410800e18eb120d729bc
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-10-31 17:57:22 -0600

    change axios config

--- src/Config/ConfigAxios.js
+++ src/Config/ConfigAxios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    //baseURL: process.env.VUE_APP_RUTA_API
-    baseURL: 'http://190.85.232.138/UMG/'
+    baseURL: process.env.VUE_APP_RUTA_API
+    // baseURL: 'http://190.85.232.138/UMG/'
 });
 
 export default clientAxios;
